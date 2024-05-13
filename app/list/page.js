cee2f9b6daf45557f7f2eba64f386e49914be049
이진석 <gkrryrwkk6@naver.com>
2024-05-13 18:04:46 +0900

    next js 이미지 넣기 cart

--- app/list/page.js
+++ app/list/page.js
@@ -1,13 +1,22 @@
+import Image from 'next/image' //없어도됨빠른 로딩시
+
+
 export default function List() {
-    return (
-      <div>
-        <h2>Products</h2>
-        <div className="food">
-          <h4>상품명 $40</h4>
-        </div>
-        <div className="food">
-          <h4>상품명 $40</h4>
-        </div>
-      </div>
-    )
-  } 
+  let 상품 = ['Tomatoes', 'Pasta', 'Coconut']
+  return (
+    <div> 
+      <h4 className="title">상품목록</h4>
+      { 
+        상품.map((a,i)=>{
+          return ( 
+            <div className="food" key={i}> 
+              <img src={"/food"+[i]+".png"} className="food-img"/>  
+              <h4>{ 상품[i] } $40 </h4>
+              {/* <h4>{a} $40</h4>  */}
+              {/* {상품[i]} 랑 {a} 랑같음 */}
+            </div>
+          )
+      }) }
+    </div>
+  )
+}
