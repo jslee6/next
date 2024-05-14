f7af80c909080816fedb754bf300b8eb2f77d143
이진석 <gkrryrwkk6@naver.com>
2024-05-14 18:07:55 +0900

    마지막 강의 보고 각각 수정되게 다시

--- app/cart/page.js
+++ app/cart/page.js
@@ -9,6 +9,8 @@ import Hi from './data.js'
 // 인포트 익스포트
 
 export default function Cart() {
+    let 장바구니 = ['Tomatoes', 'Pasta']
+
     return (
       <div>
         <h4 className="title">Cart</h4>
@@ -17,25 +19,46 @@ export default function Cart() {
           <p>$40</p>
           <p>1개</p>
         </div>
-        <Cartitem></Cartitem>
+        <Cartitem item={장바구니} ></Cartitem>
+        
+        {/* -> 장바구니 어레이변수를 가져옴 */}
+
+
         {/* 아래서 만든 카트아이템 컴포넌트 */}
+       
+        {/* props 사용방법
+
+        1. 부모-> 자식  컴포넌트에 < 자식컴포넌트 = 작명= "데이터"/>
+        2. 자식컴포넌트에서 함수에 (props
+        3. 들어갈에 {props.작명} */}
+
+
       </div>
     )
   } 
+
+
+
+
+  
+  
 
 //   컴포넌트만들기
 // 1. Function 작명(){}
 // 2. return ( 축약할 html)
 // 3. <작명><작명/> 사용
 
-  function Cartitem(){
+
+// 카트아이템 컴포넌트
+  function Cartitem(props){
     return (
         <div className="cart-item">
-        <p>상품명</p>
+        <p>상품명 {props.item[0] }</p>
         <p>$40</p>
         <p>1개</p>
         {/* <Hi></Hi>
         샘플컴포넌트 cart 페이지에서 만듬 */}
+   
     
       </div>
     )
--- app/list/page.js
+++ app/list/page.js
@@ -1,11 +1,53 @@
+'use client'
+import { useState } from 'react'
+
 import Image from 'next/image' //없어도됨빠른 로딩시
 
 
 export default function List() {
   let 상품 = ['Tomatoes', 'Pasta', 'Coconut']
+  let [수량, 수량변경] = useState([0,0,0]) 
+  //이게스테이트 만들기,클라이언트 컴포넌트에서만 사용
+  // 스테이트장점 : 스테이트 변경되면 관련 html 자동렌더링
+
+
   return (
     <div> 
       <h4 className="title">상품목록</h4>
+
+      <span>{수량[0]}</span>
+      <button onClick={()=>{ 
+        let copy = [...수량]
+        copy[0]++
+        수량변경(copy)
+       }}>+</button>
+
+       
+       {/* 스테이트 수량 개별
+       -> 변수에 복사해서 사용 */}
+
+      
+
+       <span>{수량[1]}</span>
+      <button onClick={()=>{ 
+        let copy = [...수량]
+        copy[1]++
+        수량변경(copy)
+       }}>+</button>
+
+       <span>{수량[1]}</span>
+      <button onClick={()=>{ 
+        let copy = [...수량]
+        copy[1]++
+        수량변경(copy)
+       }}>+</button>
+       
+
+
+
+
+
+
       { 
         상품.map((a,i)=>{
           return ( 
@@ -14,6 +56,15 @@ export default function List() {
               <h4>{ 상품[i] } $40 </h4>
               {/* <h4>{a} $40</h4>  */}
               {/* {상품[i]} 랑 {a} 랑같음 */}
+                <span>{수량}</span>
+              {/* 스팸태그 텍스트 그룹화 변경하거나 강조등 */}
+            
+             <span>{수량[i]}</span>
+
+        
+            
+               
+              {/* 스테이트 수량에 수량+1을 해주세요 */}
             </div>
           )
       }) }
