6d990e176f87ac3d801e2ef1915605c7746581db
이진석 <gkrryrwkk6@naver.com>
2024-05-15 01:18:15 +0900

    플러스 개별 증가

--- app/list/page.js
+++ app/list/page.js
@@ -14,57 +14,34 @@ export default function List() {
   return (
     <div> 
       <h4 className="title">상품목록</h4>
-
-      <span>{수량[0]}</span>
-      <button onClick={()=>{ 
-        let copy = [...수량]
-        copy[0]++
-        수량변경(copy)
-       }}>+</button>
-
-       
+   
        {/* 스테이트 수량 개별
        -> 변수에 복사해서 사용 */}
 
-      
-
-       <span>{수량[1]}</span>
-      <button onClick={()=>{ 
-        let copy = [...수량]
-        copy[1]++
-        수량변경(copy)
-       }}>+</button>
-
-       <span>{수량[1]}</span>
-      <button onClick={()=>{ 
-        let copy = [...수량]
-        copy[1]++
-        수량변경(copy)
-       }}>+</button>
-       
-
-
-
-
-
-
       { 
-        상품.map((a,i)=>{
+        상품.map((상품, i)=>{
           return ( 
             <div className="food" key={i}> 
-              <img src={"/food"+[i]+".png"} className="food-img"/>  
-              <h4>{ 상품[i] } $40 </h4>
-              {/* <h4>{a} $40</h4>  */}
-              {/* {상품[i]} 랑 {a} 랑같음 */}
-                <span>{수량}</span>
-              {/* 스팸태그 텍스트 그룹화 변경하거나 강조등 */}
-            
-             <span>{수량[i]}</span>
+              <img src={`/food${i}.png`} className="food-img" />  
+              <h4>{상품} $40 </h4>
+              <span>{수량[i]}</span>
 
-        
-            
-               
-              {/* 스테이트 수량에 수량+1을 해주세요 */}
+              <button onClick={() => {
+                let copy = [...수량]
+                copy[i]++
+                수량변경(copy)
+              }}>
+                +
+                </button>
+
+              <button onClick={() => {
+                let copy = [...수량]
+                copy[i]--
+                수량변경(copy)
+              }}>
+                -
+                </button>
+              {/* 수량을 복사하고 copy[i]를 1씩 감소함 -> 수량변경에 저장  */}
             </div>
           )
       }) }
